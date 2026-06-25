906364b1adc5636d25ecfeeac55359a2ab26809e
Jack Gitter <jack.a.gitter@gmail.com>
2026-06-24 20:05:57 -0400

    ok

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -1,25 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { fromPromise } from 'xstate';
+import { DataSource } from 'typeorm';
+import { authMachineDefinition } from './machine/definition';
+import { createMachine } from 'xstate';
 
 @Injectable()
 export class AuthService {
-  public sendMagicLinkActor = fromPromise(
-    async ({ input }: { input: { data: any } }) => {},
-  );
+  constructor(datasource: DataSource) {}
 
-  public validateMagicLinkActor = fromPromise(
-    async ({ input }: { input: { data: any } }) => {},
-  );
+  public sendMagicLinkActor = async () => {};
 
-  public sendOTPSMSActor = fromPromise(
-    async ({ input }: { input: { data: any } }) => {},
-  );
+  public validateMagicLinkActor = async () => {};
 
-  public validateOTPSMSActor = fromPromise(
-    async ({ input }: { input: { data: any } }) => {},
-  );
+  public sendOTPSMSActor = async () => {};
 
-  public mintSessionActor = fromPromise(
-    async ({ input }: { input: { data: any } }) => {},
-  );
+  public validateOTPSMSActor = async () => {};
+
+  public mintSessionActor = async () => {};
+
+  public createStateMachine() {
+    const definition = authMachineDefinition;
+    definition.actors.sendMagicLink = this.sendMagicLinkActor;
+    definition.actors.validateMagicLink = this.validateMagicLinkActor;
+    definition.actors.sendOTPSMS = this.sendOTPSMSActor;
+    definition.actors.validateOTPSMS = this.validateOTPSMSActor;
+    definition.actors.mintSession = this.mintSessionActor;
+
+    const actor = createMachine(authMachineDefinition);
+  }
 }
